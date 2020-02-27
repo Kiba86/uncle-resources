3f1dee559b55ee5fe7172664f2c9be8f935c75da
Kiba86 <fabrizio.pulvirenti.86@gmail.com>
2020-02-27 13:52:31 +0100

    Base file for laravel packages

---
+++ src/UncleResourceServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+namespace UncleProject\UncleResource;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
+
+class UncleResourceServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
